7e1e29af5ee4a024a07422641492194df2ef7b59
Jessua <carvajaljessua@gmail.com>
2025-04-23 02:20:28 -0400

    r

--- Practicos/practica-c/funciones.h
+++ Practicos/practica-c/funciones.h
@@ -1,15 +1,29 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
+#e1
+
 int esPar(int numero);
 
+#e2
+
 int contarDigitos(int numero);
+
+#e3
 
 typedef enum { ROJO, VERDE, AMARILLO } Semaforo;
 void mostrarEstadoSemaforo(Semaforo estado);
 
+#e4
+
 int acumular(int actual, int nuevo);
+
+#e5
 
 int sumar(int a, int b);
 int restar(int a, int b);
 int multiplicar(int a, int b);
+
+#e6
+
+void comparar(int a, int b);
