67938edc7830885b633cfeda9458a9b489fdb712
Jess1711 <carvajaljessua@gmail.com~>
2025-05-21 21:49:55 -0400

    k

--- Examen/simulacro/operaciones/opcion1.py
+++ Examen/simulacro/operaciones/opcion1.py
@@ -3,4 +3,3 @@ def contar_divisibles(N):
     for i in range(1, N + 1):
         if i % 3 == 0 or str(i).endswith('3'):
             count += 1
-    print(f"Cantidad de números entre 1 y {N} que son divisibles por 3 o terminan en 3: {count}")
